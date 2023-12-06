c36c4f8aaf46bb0b3110d1d40fc5051258110ae1
iankiema <kiemaian.ik@gmail.com>
2023-12-06 17:29:55 +0300

    Add React components and Redux for fetching data

---
+++ src/components/Greeting.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRandomGreeting } from '../redux/slices/greetingSlice';
+
+const Greeting = () => {
+  const dispatch = useDispatch();
+  const { message, isLoading, error } = useSelector((state) => state.greetings);
+
+  useEffect(() => {
+    dispatch(fetchRandomGreeting());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return <div>{message}</div>;
+};
+
+export default Greeting;
